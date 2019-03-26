1bcb582ea9ff30de58055cd1da693b3e41ddfe97
joniburn <joniburn@gmail.com>
2019-03-26 19:59:42 +0900

    排他ロックを導入

--- random_pic_server/app.py
+++ random_pic_server/app.py
@@ -3,6 +3,7 @@ from glob import glob
 import os.path
 from random import choice
 
+from filelock import FileLock
 from flask import Flask, make_response
 
 from random_pic_server.state import state as s
@@ -13,6 +14,9 @@ logger = app.logger
 
 # 対象画像の格納ディレクトリ
 PICS_DIR = os.environ['PICS_DIR']
+
+# ロックファイル
+LOCK_FILE = os.environ.get('LOCK_FILE', '.lock')
 
 # レンスポンスヘッダー定義
 RESPONSE_HEADER = {
@@ -26,26 +30,28 @@ PIC_SELECT_DELTA = timedelta(seconds=int(PIC_SELECT_INTERVAL_SEC))
 
 @app.route('/')
 def hello():
-    logger.info('Start')
+    with FileLock(LOCK_FILE, timeout=1):
+        logger.info('Start')
 
-    # 前回の画像選択から一定時間経過していない場合は最後の画像を返却
-    now = datetime.now()
-    selection_required = now - s.pic_selected_latest_time > PIC_SELECT_DELTA
-    if s.current_picture_data and not selection_required:
-        logger.info('End: not updated')
+        # 前回の画像選択から一定時間経過していない場合は最後の画像を返却
+        now = datetime.now()
+        selection_required = (
+            now - s.pic_selected_latest_time > PIC_SELECT_DELTA)
+        if s.current_picture_data and not selection_required:
+            logger.info('End: not updated')
+            return make_response(s.current_picture_data, 200, RESPONSE_HEADER)
+
+        # 返却する画像ファイルを選択
+        file_list = glob(os.path.join(PICS_DIR, '**', '*.jpg'), recursive=True)
+        pic_fname = choice(file_list)
+
+        # 画像ファイルを読み込み
+        with open(pic_fname, mode='rb') as f:
+            s.current_picture_data = f.read()
+
+        # 時刻を更新
+        s.pic_selected_latest_time = now
+
+        # レスポンスを返却
+        logger.info('End')
         return make_response(s.current_picture_data, 200, RESPONSE_HEADER)
-
-    # 返却する画像ファイルを選択
-    file_list = glob(os.path.join(PICS_DIR, '**', '*.jpg'), recursive=True)
-    pic_fname = choice(file_list)
-
-    # 画像ファイルを読み込み
-    with open(pic_fname, mode='rb') as f:
-        s.current_picture_data = f.read()
-
-    # 時刻を更新
-    s.pic_selected_latest_time = now
-
-    # レスポンスを返却
-    logger.info('End')
-    return make_response(s.current_picture_data, 200, RESPONSE_HEADER)
